7db36f6466875a43974f320bbf40e773e96992cf
fearofgod0001 <dlxo8888@gmail.com>
2023-05-30 12:22:24 +0900

    0530

--- 함수.py
+++ 함수.py
@@ -7,7 +7,7 @@
 
 #은행 계좌 개설하고 입금과 출금 예제
 def open_account(name) : 
-    pritnt(f"{name}님의 계좌가 개설 되었습니다.")
+    print(f"{name}님의 계좌가 개설 되었습니다.")
     return name
 # 입금
 def desposit(balance,input) : # 잔액과 입금 금액을 입력 받음
@@ -28,3 +28,41 @@ name = open_account("이태석")
 balance=desposit(balance, 1000)
 balance-withdraw(balance, 500)
 print(f"{name}의 잔액은 {balance} 입니다.")
+
+
+# 기본값 인자 : 함수 선언 시 매개변수에 대한 기본 값을 정의 할 수 있습니다.
+def profile(name, year =2, age=18, school="휘문고") :
+    print(f"이름 : {name}, 학교 : {school}, 학번 : {year}, 나이 : {age}")
+
+profile("이석태")
+profile("이태석")
+profile("이태석",None,22)
+
+# 가변 매개 변수 : 함수의 매개변수 앞에 (*) 붙여주면 함수의 매개변수를 몇 개를 입력 하던 함수 내에서 튜플로 인식
+def profile(name,age,*lang) :
+    print(f"이름 : {name}, 나이 : {age}",end=" ")
+    for e in lang :
+        print(e,end=" ")
+    print("\n")
+
+profile("이태석", 18,"Python","java","C","C++","React","Kotiln")
+profile("이태석2", 22,"Python","java")
+profile("이태석3", 18,"Python","java","C","C++")
+
+# 지역 변수와 전역 변수 
+# 전역변수 : 함수보다 변수의 생존 범위가 더 넓어서 리턴 값이 필요 없음
+# global 키워드를 사용하면 전여긍로 선언한 변수를 함수 내에서 사용 할 수 있음.
+knife = 10 # 칼을 10자루 준비함 
+def game(player) : 
+    global knife
+    knife -= player #게임에서 참가한 선수가 사용한 개수만큼 차감
+
+def game(player,knife) : 
+    knife -= player #게임에서 참가한 선수가 사용한 개수만큼 차감
+    print(f"남아있는 칼은 {knife} 자루 입니다.")
+
+player = int(input("경기에 참여하는 선수가 몇명 입니까? : "))
+game(player,knife)
+
+# 람다와 함수
+# 람다란? 간단한 함수의 선언과 호출을 하나의 식으로 간략히 표현, 이름 없는 함수를 의미
