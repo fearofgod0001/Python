58fe3cc9bbf8782d54a5c617b74a6814721aa52b
fearofgod0001 <dlxo8888@gmail.com>
2023-06-01 10:23:43 +0900

    0601

--- 모듈1번.py
+++ 모듈1번.py
@@ -1,7 +1,30 @@
-from math import sin, cos, tan, floor, ceil
+# from math import sin, cos, tan, floor, ceil
+# import math as m
 
-print(math.sin(1))
-print(math.cos(1))
-print(math.tan(1))
-print(math.floor(2.5))
-print(math.cos(2.5))
+# print(m.sin(1))
+# print(m.cos(1))
+# print(m.tan(1))
+# print(m.floor(2.5))
+# print(m.cos(2.5))
+
+import sys #시스템 관련 정보를 가지고 있는 모듈. 주로 매개변수를 받을 떄 사용
+import os
+#명령줄 인수 출력
+print("명령줄 인수 : ",sys.argv)
+
+#스크립트 실행 경로
+print("실행경로 : ", sys.path[0])
+
+# 프로그램 종료
+# sys.exit(0)
+
+# 현재 작업 디렉토리 가져오기
+cwd = os.getcwd()
+print("현재 작업 디렉토리 : ", cwd)
+
+# 파일 존재 여부 확인
+is_file=os.path.isfile('test.py')
+print(is_file)
+
+# 시스템 명령어 출력
+os.system("dir")
