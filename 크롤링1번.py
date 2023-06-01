8297a0068a1cc26242b8ebfa03bf64ca5c95571b
fearofgod0001 <dlxo8888@gmail.com>
2023-06-01 11:43:50 +0900

    0601

--- 크롤링1번.py
+++ 크롤링1번.py
@@ -1,11 +1,16 @@
 import requests # HTTP 요청을 보내고 응답을 받는데 사용하는 라이브러리(get, post, put, delete)
 from bs4 import BeautifulSoup #HTML 및 XML 파일에서 데이터를 추출할 떄 사용
+from flask import Flask
 
 # url = "https://www.weather.go.kr/weather/observation/currentweather.jsp"
 # html = requests.get(url).text
 # soup = BeautifulSoup(html,"html.parser")
 # print(soup)
 
+app = Flask(__name__) #__name__은 현재 모듈 이름을 의미
+@app.route()
+
+#오류
 url ="https://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp?stnId=108"
 
 #HTTP Get요청
